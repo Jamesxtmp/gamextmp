bbe0d3e646e16d236ce91d5fac313f2d998593e5
Jamesxtmp <gen22xtmp@gmail.com>
2022-06-26 21:08:23 -0500

    nuevo inicio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,51 @@
-import MapGame from './game/MapGame'
+import './css/game.css'
+import './css/noPasedElements.css'
+import { useEffect } from 'react'
 
 const App = () => {
+    function scrollButtons ( mapNode ) {
+        addEventListener('keydown', e => {
+            switch ( e.key ) {
+                case 'ArrowUp': 
+                    mapNode.scrollTop -= 10
+                    break
+                case 'ArrowDown': 
+                    mapNode.scrollTop += 10
+                    break
+                case 'ArrowRight': 
+                    mapNode.scrollLeft += 10
+                    break
+                case 'ArrowLeft': 
+                    mapNode.scrollLeft -= 10
+                    break
+                    
+                default:
+                    break
+            }
+            console.clear()
+            console.log( mapNode.scrollTop )
+            console.log( mapNode.scrollLeft )
+            console.log( mapNode.scrollWidth )
+            console.log( mapNode.scrollHeight )
+        })
+    }
+    useEffect( () => {
+        const mapNode = document.getElementById('map-view')
+        scrollButtons( mapNode )
+    }, [] )
     return(<>
-        <MapGame></MapGame>
+        <div className='map-view' id='map-view'>
+            <div className='wrap-player' >
+                <div className='player'></div>
+            </div>
+            <div className='map' id='map'>
+                <div className='no-pased' id='noPasedTop' ></div>
+                <div className='no-pased' id='noPasedBottom' ></div>
+                <div className='no-pased' id='noPasedRigth' ></div>
+                <div className='no-pased' id='noPasedLeft' ></div>
+                <div className='obstacle' ></div>
+            </div>
+        </div>
     </>)
 }
 export default App
--- src/components/tools/seterCssVar.js
+++
@@ -1,7 +0,0 @@
-export const setCssVar = ( name, value ) => {
-    if( name && value ){
-        const styleSheetToSetVars = document.styleSheets[0].cssRules[0]
-        styleSheetToSetVars.styleMap.set( name, value )
-        console.log( 'AddCssVar', name, value )
-    }
-}
